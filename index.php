4c0f8e27da33d0adf2e2b57292e4cd5d7d212b8f
Pim Verlangen <pim.verlangen@gmail.com>
2013-11-12 20:00:46 +0100

    al een heel eind jonge!

--- index.php
+++ index.php
@@ -7,8 +7,17 @@
     </head>
     <body>
         <div id="page-container">
-            <div id="main-nav"> Main Navigation </div>
-            <div id="header"> Header </div>
+            <div id="main-nav">
+                <ul class="hidden">
+                    <li id="about"><a href="#">About<a/></li>
+                    <li id="services"><a href="#">Services<a/></li>
+                    <li id="portfolio"><a href="#">Porftolio<a/></li>
+                    <li id="contact"><a href="#">Contact<a/></li>
+                </ul>
+            </div>
+            <div id="header">
+                <h1> Progress In Motion </h1>
+            </div>
             <div id="sidebar-a"> 
                 Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
                 Praesent sapien purus, ultrices a, varius ac, suscipit ut, enim. Maecenas in lectus. 
@@ -18,14 +27,26 @@
                 purus. Suspendisse mattis nunc vestibulum ligula. In hac habitasse platea dictumst.
             </div>
             <div id="content"> 
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
-                Praesent sapien purus, ultrices a, varius ac, suscipit ut, enim. Maecenas in lectus. 
-                Donec in sapien in nibh rutrum gravida. Sed ut mauris. Fusce malesuada enim vitae lacus 
-                euismod vulputate. Nullam rhoncus mauris ac metus. Maecenas vulputate aliquam odio. 
-                Duis scelerisque justo a pede. Nam augue lorem, semper at, porta eget, placerat eget, 
-                purus. Suspendisse mattis nunc vestibulum ligula. In hac habitasse platea dictumst.
+                <h2>About</h2>
+                <p><strong>Progress In Motion</strong> is a *company description*.</p>
+                <h2>Contact us</h2>
+                <p>Phone:   (07) 853 6060<br />
+                    Fax:     (07) 853 6060<br />
+                    Email:   <a href="mailto:info@enlighten.co.nz">info@enlighten.co.nz</a><br />
+                    P.O Box: 14159, Hamilton, New Zealand</p>
+                <p><a href="#">More contact information…</a></p>
+
             </div>
-            <div id="footer"> Footer </div>
+            <div id="footer">
+                <div id="altnav">
+                    <a href="#">About</a> - 
+                    <a href="#">Services</a> - 
+                    <a href="#">Portfolio</a> - 
+                    <a href="#">Contact Us</a> - 
+                    <a href="#">Terms of Trade</a>
+                </div>
+                Copyright © Progress In Motion
+            </div>
         </div>
     </body>
 </html>
