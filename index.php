1cf6d1695ffc4768126810cab26022ad2f9bfe3e
Pim Verlangen <pim.verlangen@gmail.com>
2013-11-12 14:25:48 +0100

    initial

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
+        <link rel="stylesheet" type="text/css" href="css/style.css"/>
+        <title></title>
+    </head>
+    <body>
+        <div id="page-container">
+            <div id="main-nav"> Main Navigation </div>
+            <div id="header"> Header </div>
+            <div id="sidebar-a"> 
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
+                Praesent sapien purus, ultrices a, varius ac, suscipit ut, enim. Maecenas in lectus. 
+                Donec in sapien in nibh rutrum gravida. Sed ut mauris. Fusce malesuada enim vitae lacus 
+                euismod vulputate. Nullam rhoncus mauris ac metus. Maecenas vulputate aliquam odio. 
+                Duis scelerisque justo a pede. Nam augue lorem, semper at, porta eget, placerat eget, 
+                purus. Suspendisse mattis nunc vestibulum ligula. In hac habitasse platea dictumst.
+            </div>
+            <div id="content"> 
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
+                Praesent sapien purus, ultrices a, varius ac, suscipit ut, enim. Maecenas in lectus. 
+                Donec in sapien in nibh rutrum gravida. Sed ut mauris. Fusce malesuada enim vitae lacus 
+                euismod vulputate. Nullam rhoncus mauris ac metus. Maecenas vulputate aliquam odio. 
+                Duis scelerisque justo a pede. Nam augue lorem, semper at, porta eget, placerat eget, 
+                purus. Suspendisse mattis nunc vestibulum ligula. In hac habitasse platea dictumst.
+            </div>
+            <div id="footer"> Footer </div>
+        </div>
+    </body>
+</html>
