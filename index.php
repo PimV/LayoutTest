5a7b62be91ef31bea76f4bdb75fa8595a29a2454
PimGame <PimGame@WINDOWS-JHOM3EU>
2013-11-13 06:24:49 +0100

    headers gemaakt en 'headings' gemaakt

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
@@ -8,7 +8,7 @@
     <body>
         <div id="page-container">
             <div id="main-nav">
-                <ul class="hidden">
+                <ul >
                     <li id="about"><a href="#">About<a/></li>
                     <li id="services"><a href="#">Services<a/></li>
                     <li id="portfolio"><a href="#">Porftolio<a/></li>
@@ -16,25 +16,37 @@
                 </ul>
             </div>
             <div id="header">
-                <h1> Progress In Motion </h1>
+                <h1><img src ="images/headings/header_full.png" width ="760" height ="60" alt="Progress In Motion"/></h1>
             </div>
             <div id="sidebar-a"> 
-                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
-                Praesent sapien purus, ultrices a, varius ac, suscipit ut, enim. Maecenas in lectus. 
-                Donec in sapien in nibh rutrum gravida. Sed ut mauris. Fusce malesuada enim vitae lacus 
-                euismod vulputate. Nullam rhoncus mauris ac metus. Maecenas vulputate aliquam odio. 
-                Duis scelerisque justo a pede. Nam augue lorem, semper at, porta eget, placerat eget, 
-                purus. Suspendisse mattis nunc vestibulum ligula. In hac habitasse platea dictumst.
+                <div class="padding">
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam gravida enim ut risus. 
+                    Praesent sapien purus, ultrices a, varius ac, suscipit ut, enim. Maecenas in lectus. 
+                    Donec in sapien in nibh rutrum gravida. Sed ut mauris. Fusce malesuada enim vitae lacus 
+                    euismod vulputate. Nullam rhoncus mauris ac metus. Maecenas vulputate aliquam odio. 
+                    Duis scelerisque justo a pede. Nam augue lorem, semper at, porta eget, placerat eget, 
+                    purus. Suspendisse mattis nunc vestibulum ligula. In hac habitasse platea dictumst.
+                </div>
             </div>
             <div id="content"> 
-                <h2>About</h2>
-                <p><strong>Progress In Motion</strong> is a *company description*.</p>
-                <h2>Contact us</h2>
-                <p>Phone:   (07) 853 6060<br />
-                    Fax:     (07) 853 6060<br />
-                    Email:   <a href="mailto:info@enlighten.co.nz">info@enlighten.co.nz</a><br />
-                    P.O Box: 14159, Hamilton, New Zealand</p>
-                <p><a href="#">More contact information…</a></p>
+                <div class="padding">
+                    <h2><img src ="images/headings/about.png" width ="54" height ="20" alt="About"/></h2>
+                    <p><strong>Progress In Motion</strong> is a specialized in
+                        application development. Currently, this website is under 
+                        development and will be available soon. 
+                        <br/>
+                        <br/>
+
+                        You can always stay tuned, and check the 
+                        <strong> Progress In Motion </strong>!
+
+                    </p>
+                    <h2><img src ="images/headings/contact.png" width ="98" height ="20" alt="Contact"/></h2>
+                    <p>Phone:   (+31) 6 39 04 30 43<br />                       
+                        Email:   <a href="mailto:progressinmotion@gmail.com">progressinmotion@gmail.com</a><br />
+                        Postal Code: 5422CL Gemert, Netherlands</p>
+                    <p><a href="#">More contact information…</a></p>
+                </div>
 
             </div>
             <div id="footer">
